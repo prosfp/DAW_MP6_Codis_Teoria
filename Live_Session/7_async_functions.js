61c66a93cffca2b881354b74f3d8fd90c8a3639e
prosfp <110301233+prosfp@users.noreply.github.com>
2024-01-23 15:16:57 +0100

    update_async

--- Live_Session/7_async_functions.js
+++ Live_Session/7_async_functions.js
@@ -1,35 +1,31 @@
-// Async és una mà de pintura sobre les promeses, ens ajuda a "netejar" encara una mica més el procés asíncron
-// NO AFEGEIG RES NOU A NIVELL DE LÒGICA!
+// Async és una mà de pintura sobre les promeses, ens ajuda a "netejar" encara
+// una mica més el procés asíncron
+// NO AFEGEIG RES NOU A NIVELL DE PROMESES
 
 // 1. Les funcions "Async" sempre retornen "Promise"
 // 2. Quan la funció retorna un valor, la promesa es resoldrà ("resolve") amb aquest valor
 // 3. Si en canvi llança una excepció (throw exception), la promesa serà rebutjada ("rejected")
 
 // const hola = async () => {
-//   throw new Error('ErrorPablo', 'Oups, error!')
-//   //throw 'Error!!!'
-//   //Qualssevol error (syntax, reference...) serà entès com un "rejected" per la promesa
-//   return 'hello world'
+//   throw new Error('El meu error!')
+//   //return 'hello world'
 // }
-
-// // Si retorno un valor, la promesa es resol, altrament si es dona un error, es rebutja.
 
 // hola()
 //   .then(data => {
-//     console.log(`PROMISE RESOLVED WITH ${data}`)
+//     console.log(data)
 //   })
 //   .catch(err => {
-//     console.log('PROMISE REJECTED!')
-//     console.log(err.stack)
+//     console.log(err)
 //   })
 
-// Anem a fer un exemple més real
+// EXEMPLE MÉS REALISTA
 
-const login = async (username, password) => {
-  if (!username || !password) throw 'Manquen credencials'
-  if (password === '1234') return 'Benvingut!'
-  throw 'invalid password'
-}
+// const login = async (username, password) => {
+//   if (!username || !password) throw 'Manquen credencials'
+//   if (password === '1234') return 'Benvingut!'
+//   throw 'invalid password'
+// }
 
 // login('pablo', '1234')
 //   .then(msg => {
@@ -41,13 +37,7 @@ const login = async (username, password) => {
 //     console.log(err)
 //   })
 
-// Vale, però de moment aquí no hi ha res asincron no?
-// Async --> retorna promise
-// Retorna resolve/rejected amb els valors corresponents
-
-// D'acord, anem a veure "Await"!
-// El farem servir DINS d'una funció Async!
-// Anem a recuperar el cas dels colors:
+// ANEM A VEURE UN CAS REALMENT ASÍNCRON
 
 const delayedColorChange = (color, delay) => {
   return new Promise((resolve, reject) => {
@@ -58,7 +48,7 @@ const delayedColorChange = (color, delay) => {
   })
 }
 
-//   delayedColorChange('red', 1000)
+// delayedColorChange('red', 1000)
 //   .then(() => delayedColorChange('orange', 1000))
 //   .then(() => delayedColorChange('blue', 1000))
 //   .then(() => delayedColorChange('violet', 1000))
@@ -67,24 +57,15 @@ const delayedColorChange = (color, delay) => {
 
 async function rainbow() {
   await delayedColorChange('red', 1000)
-  await delayedColorChange('yellow' ,1000')
+  await delayedColorChange('yellow', 1000)
   await delayedColorChange('orange', 1000)
-  console.log('Hi!')
   await delayedColorChange('blue', 1000)
   await delayedColorChange('violet', 1000)
-  //finalment posaríem el return
   return 'All done'
 }
 
-//rainbow()
-
 // És bastant pràctic no haver-me de preocupar per encadenar les diferents callbacks, .thens, returning promises...
 // Simplement espero "await" promises per continuar fent la meva feina.
-
-// Amb el -return "All done"- gestiono el meu .then fora de rainbow (similarment al catch final)
-//rainbow().then(() => console.log('End of Rainbow'))
-
-// Puc inclus encapsular tot aquest seguit d'accions sota una nova promesa i ni tan sols he de fer ús del then com a tal.
 
 async function printRainbow() {
   await rainbow()
@@ -93,13 +74,14 @@ async function printRainbow() {
 
 //printRainbow()
 
-// Anem a veure com resoldríem el cas que les promeses siguin Rejected
+// Anem a veure com resoldre el cas que les promeses siguin Refusades
 
-// Tornem a l'exemple d'una petició "fake" que triga un temps random retornar
+// Tornem a l'exemple de la petició "fake"
 
 const fakeRequest = url => {
   return new Promise((resolve, reject) => {
     const delay = Math.floor(Math.random() * 4500) + 500
+    console.log(delay)
     setTimeout(() => {
       if (delay > 3000) {
         reject("Temps d'espera exhaurit! :(")
@@ -110,23 +92,18 @@ const fakeRequest = url => {
   })
 }
 
-// ABANS D'ENTRAR A LA GESTIÓ AMB TRY/CATCH:
-
-// async function makeTwoRequests() {
-//   let data1 = await fakeRequest('/page1')
-//   console.log(data1)
-// }
+// Entra en joc l'ús de try/catch
 
 async function makeTwoRequests() {
   try {
-    let data1 = await fakeRequest('/page1')
+    let data1 = await fakeRequest('meva_url_1')
     console.log(data1)
-    let data2 = await fakeRequest('/page2')
+    let data2 = await fakeRequest('meva_url_2')
     console.log(data2)
-    // Si la petició es rebutja podem gestionar també què fer a la nostra funció Async amb un try/catch
   } catch (e) {
-    console.log('ERROR!')
-    console.log('Error is:', e)
+    // En cas que alguna de les peticions es rebutgi
+    console.log('ERROR')
+    console.log('Existeix un error:', e)
   }
 }
 
@@ -138,5 +115,4 @@ makeTwoRequests()
 // 2. async/await només afecta al receptor, com gestionem el retorn de la promesa.
 // 3. Pots fer "await" a qualssevol funció que retorna una Promesa (fetch)
 // 4. Qualssevol funció es pot converitr a async.
-// 5. Totes les funcions async, immediatament passarà a retornar una promesa.
-// 6. Error handling amb try/catch
+// 5. Error handling amb try/catch
